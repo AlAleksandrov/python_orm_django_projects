3931caf5df15e38118f4f64c549f0fb2a7bd445d
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-04 18:48:05 +0200

    Update settings.py

--- lab_I_ORMIntroduction/orm_skeleton/settings.py
+++ lab_I_ORMIntroduction/orm_skeleton/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "my_first_django_db_lab",
         "USER": "postgres",
-        "PASSWORD": "@9c5m6I5a3",
+        "PASSWORD": "********",
         "HOST": "localhost",
         "PORT": "5432",
     }
