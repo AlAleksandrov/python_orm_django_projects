259f2c8c9bbfe577e2b727a82e0fb6b07f8572e5
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-04 18:46:04 +0200

    Update settings.py

--- exercise_V_WorkingWithQueriesInDjango/orm_skeleton/settings.py
+++ exercise_V_WorkingWithQueriesInDjango/orm_skeleton/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "exercise_5_working_with_queries_in_django",
         "USER": "postgres",
-        "PASSWORD": "@9c5m6I5a3",
+        "PASSWORD": "********",
         "HOST": "localhost",
         "PORT": "5432",
     }
