fa16b52bc32668ed205b41f961744f1b9b29e594
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-04 18:49:30 +0200

    Update settings.py

--- lab_V_WorkingWithQueriesInDjango/orm_skeleton/settings.py
+++ lab_V_WorkingWithQueriesInDjango/orm_skeleton/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "lab_5_working_with_queries_in_django",
         "USER": "postgres",
-        "PASSWORD": "@9c5m6I5a3",
+        "PASSWORD": "********",
         "HOST": "localhost",
         "PORT": "5432",
     }
