5ca96de7ed087d349662ccb66eae252cb3755ce8
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-13 15:48:59 +0200

    Update models.py

--- lab_IX_AdvancedQueriesInDjango/main_app/models.py
+++ lab_IX_AdvancedQueriesInDjango/main_app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.db.models import QuerySet
 
+
+class ProductManager(models.Manager):
+
+    def available_products(self):
+        return self.filter(is_available=True)
+
+    def available_products_in_category(self, category_name: str) -> QuerySet:
+        return self.filter(category__name=category_name, is_available=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -11,6 +20,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return f"{self.category.name}: {self.name}"
