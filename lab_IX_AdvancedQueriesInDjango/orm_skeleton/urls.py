5259c429bfff717c3d9d3c1f92d22facffb84395
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-13 15:49:21 +0200

    Update urls.py

--- lab_IX_AdvancedQueriesInDjango/orm_skeleton/urls.py
+++ lab_IX_AdvancedQueriesInDjango/orm_skeleton/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Добавете вашите URL patterns тук
 ]
+
+# Добавяне на Django Debug Toolbar (само за DEBUG режим)
+from django.conf import settings
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
