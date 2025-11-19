0526d1f7cd260655097f05a2337326f7afbe4701
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-11-19 20:51:15 +0200

    Create querysets.py

---
+++ exam_prep_II/main_app/querysets.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import QuerySet
+from django.db.models.aggregates import Count
+
+
+class ProfileQuerySet(models.QuerySet):
+    def get_regular_customers(self) -> QuerySet:
+        return self.annotate(
+            count_orders=Count('order')
+        ). filter(
+            count_orders__gt=2,
+        ).order_by(
+            '-count_orders'
+        )
